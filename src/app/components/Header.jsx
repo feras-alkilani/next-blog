ee7aac24adfc6eea045a7f08c20de5f6d4124e02
Feras Alkilani <eng.feras90@gmail.com>
2025-03-16 17:58:10 +0100

    Create Header section

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -2,35 +2,10 @@
 import { Button, Navbar, TextInput } from "flowbite-react";
 import Link from "next/link";
 import { AiOutlineSearch } from "react-icons/ai";
-import { FaMoon, FaSun } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
 import { usePathname } from "next/navigation";
-// import { useTheme } from "next-themes";
-// import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-// import { dark, light } from "@clerk/themes";
-// import { useEffect, useState } from "react";
-// import { useRouter, useSearchParams } from "next/navigation";
 export default function Header() {
   const path = usePathname();
-  //   const { theme, setTheme } = useTheme();
-  //   const router = useRouter();
-  //   const [searchTerm, setSearchTerm] = useState("");
-  //   const searchParams = useSearchParams();
-
-  //   const handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     const urlParams = new URLSearchParams(searchParams);
-  //     urlParams.set("searchTerm", searchTerm);
-  //     const searchQuery = urlParams.toString();
-  //     router.push(`/search?${searchQuery}`);
-  //   };
-
-  //   useEffect(() => {
-  //     const urlParams = new URLSearchParams(searchParams);
-  //     const searchTermFromUrl = urlParams.get("searchTerm");
-  //     if (searchTermFromUrl) {
-  //       setSearchTerm(searchTermFromUrl);
-  //     }
-  //   }, [searchParams]);
   return (
     <Navbar className="border-b-2">
       <Link
@@ -38,41 +13,30 @@ export default function Header() {
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
         <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-          Feras&apos;s
+          Feras's
         </span>
         Blog
       </Link>
-      <form
-      //   onSubmit={handleSubmit}
-      >
+      <form>
         <TextInput
           type="text"
           placeholder="Search..."
           rightIcon={AiOutlineSearch}
           className="hidden lg:inline"
-          //   value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button
-          className="w-12 h-10 hidden sm:inline"
-          color="gray"
-          pill
-          onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-        >
-          {/* {theme === "light" ? <FaSun /> : <FaMoon />} */}
+        <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
+          <FaMoon />
         </Button>
-
         <Link href="/sign-in">
           <Button gradientDuoTone="purpleToBlue" outline>
             Sign In
           </Button>
         </Link>
-        {/* </SignedOut> */}
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
