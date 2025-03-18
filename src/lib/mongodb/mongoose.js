7b7a0c2a9fe4e370a3193dc15be38df5bac724de
Feras Alkilani <eng.feras90@gmail.com>
2025-03-19 00:17:23 +0100

    Add MongoDB and save users from Clerk

---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { collectSegmentData } from "next/dist/server/app-render/collect-segment-data";
+
+let initialized = false;
+
+export const connect = async () => {
+  mongoose.set("strictQuery", true);
+  if (initialized) {
+    console.log("Already connected to MongoDB");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "next-blog",
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connected To MongoDB");
+    initialized = true;
+  } catch (error) {
+    console.log("Error Connecting To MongoDB", error);
+  }
+};
