e7c3085d551779ae761d2a0c29ae0e37b4f2e40e
Feras Alkilani <eng.feras90@gmail.com>
2025-03-19 18:24:49 +0100

    fix some problems with fonts

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI
+  }
+};
 
 export default nextConfig;
